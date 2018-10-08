9c469091b67f7c32c97b5b92150821cd2180ed54
Esa Firman <esafirm21@gmail.com>
2018-10-09 00:40:39 +0700

    Exlucde coverage

--- gulpfile.js
+++ gulpfile.js
@@ -5,58 +5,64 @@ var eslint = require('gulp-eslint');
 var excludeGitignore = require('gulp-exclude-gitignore');
 var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul');
-var nsp = require('gulp-nsp');
 var plumber = require('gulp-plumber');
 var coveralls = require('gulp-coveralls');
 
 gulp.task('static', function () {
-  return gulp.src('**/*.js')
+  return gulp
+    .src('**/*.js')
     .pipe(excludeGitignore())
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
 });
 
-gulp.task('nsp', function (cb) {
-  nsp({package: path.resolve('package.json')}, cb);
-});
-
 gulp.task('pre-test', function () {
-  return gulp.src('generators/**/index.js')
+  return gulp
+    .src('generators/**/index.js')
     .pipe(excludeGitignore())
-    .pipe(istanbul({
-      includeUntested: true
-    }))
+    .pipe(
+      istanbul({
+        includeUntested: true
+      })
+    )
     .pipe(istanbul.hookRequire());
 });
 
-gulp.task('test', gulp.series('pre-test', cb => {
-  var mochaErr;
+gulp.task(
+  'test',
+  gulp.series('pre-test', cb => {
+    var mochaErr;
 
-  gulp.src('test/**/*.js')
-    .pipe(plumber())
-    .pipe(mocha({reporter: 'spec'}))
-    .on('error', function (err) {
-      mochaErr = err;
-    })
-    .pipe(istanbul.writeReports())
-    .on('end', function () {
-      cb(mochaErr);
-    });
-}));
+    gulp
+      .src('test/**/*.js')
+      .pipe(plumber())
+      .pipe(mocha({reporter: 'spec'}))
+      .on('error', function (err) {
+        mochaErr = err;
+      })
+      .pipe(istanbul.writeReports())
+      .on('end', function () {
+        cb(mochaErr);
+      });
+  })
+);
 
 gulp.task('watch', function () {
   gulp.watch(['generators/**/*.js', 'test/**'], ['test']);
 });
 
-gulp.task('coveralls', gulp.series('test', () => {
-  if (!process.env.CI) {
-    return;
-  }
+gulp.task(
+  'coveralls',
+  gulp.series('test', () => {
+    if (!process.env.CI) {
+      return;
+    }
 
-  return gulp.src(path.join(__dirname, 'coverage/lcov.info'))
-    .pipe(coveralls());
-}));
+    return gulp
+      .src(path.join(__dirname, 'coverage/lcov.info'))
+      .pipe(coveralls());
+  })
+);
 
-gulp.task('prepublish', gulp.series('nsp'));
-gulp.task('default', gulp.series('static', 'test', 'coveralls'));
+gulp.task('default', gulp.series('static', 'test'));
